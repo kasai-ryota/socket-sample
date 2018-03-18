a4f9828ee7009719c10177d6c8202835196cad9e
kasai-ryota <kasai-ryota@kayac.com>
2018-03-18 21:17:38 +0900

    simple communication implementation

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const app = require('express')();
-const http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require("express")();
+const http = require("http").Server(app);
+var io = require("socket.io")(http);
 const PORT = process.env.PORT || 3000;
 
 app.get(`/`, (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("chat message", function(msg) {
+    io.emit("chat message", msg);
+    console.log("message: " + msg);
+  });
+
+  socket.on("disconnect", function() {
+    console.log("disconnected");
+  });
 });
 
 http.listen(PORT, () => {
